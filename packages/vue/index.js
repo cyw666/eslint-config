ac8e7fa2a7b8777c48917d4ca024f41998f54c37
cyw <cyw19931210@163.com>
2022-09-27 16:06:49 +0800

    fix: update vue lint config

--- packages/vue/index.js
+++ packages/vue/index.js
@@ -3,5 +3,5 @@ const rules = require('./rules')
 module.exports = {
   parser: 'vue-eslint-parser',
   extends: ['plugin:vue/vue3-recommended', '@cyw666/eslint-config-basic'],
-  rules: rules,
+  rules,
 }
