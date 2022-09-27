6babbc71831fbfa958fbb79a0285a43ef1ba672e
cyw <cyw19931210@163.com>
2022-09-27 17:46:40 +0800

    fix: update typescript lint config

--- packages/typescript/index.js
+++ packages/typescript/index.js
@@ -6,9 +6,6 @@ module.exports = {
     '@cyw666/eslint-config-basic',
   ],
   parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: './tsconfig.json',
-  },
   plugins: ['@typescript-eslint'],
   rules: {
     '@typescript-eslint/no-explicit-any': 'off',
--- packages/typescript/src/test.ts
+++
@@ -1,7 +0,0 @@
-function add(params: string) {
-  console.log(params)
-}
-
-let sub = 1
-
-let asdf
